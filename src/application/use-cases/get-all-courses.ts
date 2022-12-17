996cbc81e2d746bc78e12eda26a2c9105b713fe1
felipesanderp <felipe.panisson@hotmail.com>
2022-12-17 09:36:48 -0300

    use case to return all curses

---
+++ src/application/use-cases/get-all-courses.ts
@@ -0,0 +1,20 @@
+import { CoursesRepository } from '../repositories/courses-repository';
+import { Injectable } from '@nestjs/common';
+import { Course } from '../entities/course';
+
+interface GetAllCoursesResponse {
+  courses: Course[];
+}
+
+@Injectable()
+export class GetAllCourses {
+  constructor(private coursesRepository: CoursesRepository) {}
+
+  async execute(): Promise<GetAllCoursesResponse> {
+    const courses = await this.coursesRepository.findAllCourses();
+
+    return {
+      courses,
+    };
+  }
+}
