722f195ec31fbe0961a1566ba55dcc5a50b61781
felipesanderp <felipe.panisson@hotmail.com>
2022-12-17 09:00:33 -0300

    method to return all courses

--- src/application/repositories/courses-repository.ts
+++ src/application/repositories/courses-repository.ts
@@ -3,4 +3,5 @@ import { Course } from '../entities/course';
 export abstract class CoursesRepository {
   abstract create(course: Course): Promise<void>;
   abstract findByTitle(title: string): Promise<Course | null>;
+  abstract findAllCourses(): Promise<Course[]>;
 }
--- test/repositories/in-memory-courses-repository.ts
+++ test/repositories/in-memory-courses-repository.ts
@@ -4,6 +4,10 @@ import { CoursesRepository } from '@application/repositories/courses-repository'
 export class InMemoryCoursesRepository implements CoursesRepository {
   public courses: Course[] = [];
 
+  async findAllCourses(): Promise<Course[]> {
+    return this.courses;
+  }
+
   async findByTitle(title: string): Promise<Course | null> {
     const course = this.courses.find((item) => item.title === title);
 
