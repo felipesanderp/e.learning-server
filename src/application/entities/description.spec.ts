ff420afdd49c8a118e287050b682e46574496b0c
felipesanderp <felipe.panisson@hotmail.com>
2022-12-19 10:59:15 -0300

    refactor test title

--- src/application/entities/description.spec.ts
+++ src/application/entities/description.spec.ts
@@ -1,6 +1,6 @@
 import { Description } from './description';
 
-describe('Course description', () => {
+describe('Description', () => {
   it('should be able to create a course description', () => {
     const description = new Description('This is a course description');
 
