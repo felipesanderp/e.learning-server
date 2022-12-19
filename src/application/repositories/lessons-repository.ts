ac5101bc33125552d72626a08c67982d4185a056
felipesanderp <felipe.panisson@hotmail.com>
2022-12-19 14:27:59 -0300

    find by name method for lesson repository

--- src/application/repositories/lessons-repository.ts
+++ src/application/repositories/lessons-repository.ts
@@ -2,4 +2,5 @@ import { Lesson } from '@application/entities/lesson';
 
 export abstract class LessonsRepository {
   abstract create(lesson: Lesson): Promise<void>;
+  abstract findByName(name: string): Promise<Lesson | null>;
 }
--- test/repositories/in-memory-lessons-repository.ts
+++ test/repositories/in-memory-lessons-repository.ts
@@ -4,6 +4,16 @@ import { LessonsRepository } from '@application/repositories/lessons-repository'
 export class InMemoryLessonsRepository implements LessonsRepository {
   public lessons: Lesson[] = [];
 
+  async findByName(name: string): Promise<Lesson | null> {
+    const lesson = this.lessons.find((item) => item.name === name);
+
+    if (!lesson) {
+      return null;
+    }
+
+    return lesson;
+  }
+
   async create(lesson: Lesson): Promise<void> {
     this.lessons.push(lesson);
   }
