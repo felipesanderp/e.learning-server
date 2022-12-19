517c6df271749bcaf4bcf1a8cd2665223925a5b3
felipesanderp <felipe.panisson@hotmail.com>
2022-12-19 15:48:06 -0300

    remove course use case and tests

---
+++ src/application/use-cases/courses/remove-course.spec.ts
@@ -0,0 +1,35 @@
+import { Description } from '@application/entities/description';
+import { InMemoryCoursesRepository } from '@test/repositories/in-memory-courses-repository';
+import { CourseNotFound } from '../errors/course-not-found';
+import { CreateCourse } from './create-course';
+import { RemoveCourse } from './remove-course';
+
+describe('Remove Course', () => {
+  it('should be able to remove a course', async () => {
+    const coursesRepository = new InMemoryCoursesRepository();
+    const createCourse = new CreateCourse(coursesRepository);
+    const removeCourse = new RemoveCourse(coursesRepository);
+
+    const { course } = await createCourse.execute({
+      title: 'title-example-1',
+      slug: 'slug-example',
+      description: new Description('course-description'),
+      imageURL: 'image-url-example',
+    });
+
+    await removeCourse.execute(course.id);
+
+    expect(coursesRepository.courses).toHaveLength(0);
+  });
+
+  it('should not be able to remove a course that does not exist', async () => {
+    const coursesRepository = new InMemoryCoursesRepository();
+    const removeCourse = new RemoveCourse(coursesRepository);
+
+    const fakeId = 'fake-course-id';
+
+    expect(async () => {
+      await removeCourse.execute(fakeId);
+    }).rejects.toThrow(CourseNotFound);
+  });
+});
--- src/application/use-cases/courses/remove-course.ts
+++ src/application/use-cases/courses/remove-course.ts
@@ -2,19 +2,13 @@ import { CoursesRepository } from '@application/repositories/courses-repository'
 import { Injectable } from '@nestjs/common';
 import { CourseNotFound } from '../errors/course-not-found';
 
-interface RemoveCourseRequest {
-  id: string;
-}
-
 type RemoveCourseResponse = void;
 
 @Injectable()
 export class RemoveCourse {
   constructor(private coursesRepository: CoursesRepository) {}
 
-  async execute(request: RemoveCourseRequest): Promise<RemoveCourseResponse> {
-    const { id } = request;
-
+  async execute(id: string): Promise<RemoveCourseResponse> {
     const courseExists = await this.coursesRepository.findById(id);
 
     if (!courseExists) {
