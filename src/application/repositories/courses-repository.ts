cf871080df0efb92a8d332095280ef25b0a99079
felipesanderp <felipe.panisson@hotmail.com>
2022-12-19 15:11:00 -0300

    implement methods to findById and remove a course ' '

--- src/application/repositories/courses-repository.ts
+++ src/application/repositories/courses-repository.ts
@@ -4,4 +4,6 @@ export abstract class CoursesRepository {
   abstract create(course: Course): Promise<void>;
   abstract findByTitle(title: string): Promise<Course | null>;
   abstract findAllCourses(): Promise<Course[]>;
+  abstract findById(id: string): Promise<Course | null>;
+  abstract remove(id: string): Promise<void>;
 }
--- test/repositories/in-memory-courses-repository.ts
+++ test/repositories/in-memory-courses-repository.ts
@@ -2,6 +2,16 @@ import { Course } from '@application/entities/course';
 import { CoursesRepository } from '@application/repositories/courses-repository';
 
 export class InMemoryCoursesRepository implements CoursesRepository {
+  async findById(id: string): Promise<Course | null> {
+    const course = this.courses.find((item) => item.id === id);
+
+    if (!course) {
+      return null;
+    }
+
+    return course;
+  }
+
   public courses: Course[] = [];
 
   async findAllCourses(): Promise<Course[]> {
@@ -20,5 +30,11 @@ export class InMemoryCoursesRepository implements CoursesRepository {
 
   async create(course: Course): Promise<void> {
     this.courses.push(course);
+  }
+
+  async remove(id: string): Promise<void> {
+    const courseIndex = this.courses.findIndex((item) => item.id === id);
+
+    this.courses.splice(courseIndex, 1);
   }
 }
