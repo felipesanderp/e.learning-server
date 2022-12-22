bbe80654b68ab671cc04ed4e884e52a82897e9ac
Felipe Sander <felipe.panisson@hotmail.com>
2022-12-22 15:10:14 -0300

    implements methods

--- test/repositories/in-memory-enrollments-repository.ts
+++ test/repositories/in-memory-enrollments-repository.ts
@@ -4,7 +4,29 @@ import { EnrollmentsRepository } from '@application/repositories/enrollments-rep
 export class InMemoryEnrollmentsRepository implements EnrollmentsRepository {
   public enrollments: Enrollment[] = [];
 
+  async findById(enrollmentId: string): Promise<Enrollment | null> {
+    const enrollment = this.enrollments.find(
+      (item) => item.id === enrollmentId,
+    );
+
+    if (!enrollment) {
+      return null;
+    }
+
+    return enrollment;
+  }
+
   async create(enrollment: Enrollment): Promise<void> {
     this.enrollments.push(enrollment);
+  }
+
+  async save(enrollment: Enrollment): Promise<void> {
+    const enrollmentIndex = this.enrollments.findIndex(
+      (item) => item.id === enrollment.id,
+    );
+
+    if (enrollmentIndex >= 0) {
+      this.enrollments[enrollmentIndex] = enrollment;
+    }
   }
 }
