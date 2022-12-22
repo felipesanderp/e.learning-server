2cd77cc86d6f2f915ec4edee8c97e4a2e0155626
Felipe Sander <felipe.panisson@hotmail.com>
2022-12-22 15:07:59 -0300

    add methods to enrollments repository

--- src/application/repositories/enrollments-repository.ts
+++ src/application/repositories/enrollments-repository.ts
@@ -2,4 +2,6 @@ import { Enrollment } from '@application/entities/enrollment';
 
 export abstract class EnrollmentsRepository {
   abstract create(enrollment: Enrollment): Promise<void>;
+  abstract findById(enrollmentId: string): Promise<Enrollment | null>;
+  abstract save(enrollment: Enrollment): Promise<void>;
 }
