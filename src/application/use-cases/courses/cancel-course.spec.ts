44609db157fd9faef75c5626fe211423f742cacd
Felipe Sander <felipe.panisson@hotmail.com>
2022-12-27 09:00:25 -0300

    cancel course tests

--- src/application/use-cases/courses/cancel-course.spec.ts
+++ src/application/use-cases/courses/cancel-course.spec.ts
@@ -1,5 +1,6 @@
 import { makeCourse } from '@test/factories/course-factory';
 import { InMemoryCoursesRepository } from '@test/repositories/in-memory-courses-repository';
+import { CourseNotFound } from '../errors/course-not-found';
 import { CancelCourse } from './cancel-course';
 
 describe('Cancel Course', () => {
@@ -14,5 +15,14 @@ describe('Cancel Course', () => {
     await cancelCourse.execute(course.id);
 
     expect(coursesRepository.courses[0].canceledAt).toEqual(expect.any(Date));
+  });
+
+  it('should not be able to cancel a non existing course', () => {
+    const coursesRepository = new InMemoryCoursesRepository();
+    const cancelCourse = new CancelCourse(coursesRepository);
+
+    expect(() => {
+      return cancelCourse.execute('fake-course-id');
+    }).rejects.toThrow(CourseNotFound);
   });
 });
