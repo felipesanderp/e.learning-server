9a7eaee58db4d5210e660bd30924b426d7cfd8c1
felipesanderp <felipe.panisson@hotmail.com>
2022-12-29 14:13:02 -0300

    update course use case and tests

---
+++ src/application/use-cases/courses/update-course.spec.ts
@@ -0,0 +1,41 @@
+import { makeCourse } from '@test/factories/course-factory';
+import { InMemoryCoursesRepository } from '@test/repositories/in-memory-courses-repository';
+import { CourseNotFound } from '../errors/course-not-found';
+import { UpdateCourse } from './update-course';
+
+describe('Update Course', () => {
+  it('should be able to update a course', async () => {
+    const coursesRepository = new InMemoryCoursesRepository();
+    const updateCourse = new UpdateCourse(coursesRepository);
+
+    const createCourse = makeCourse();
+
+    await coursesRepository.create(createCourse);
+
+    await updateCourse.execute({
+      courseId: createCourse.id,
+      course: {
+        title: 'New Course Title',
+      },
+    });
+
+    expect(coursesRepository.courses[0]).toEqual(
+      expect.objectContaining({
+        title: 'New Course Title',
+        slug: 'new-course-title',
+      }),
+    );
+  });
+
+  it('should not be able to update a non existing Course', () => {
+    const coursesRepository = new InMemoryCoursesRepository();
+    const updateCourse = new UpdateCourse(coursesRepository);
+
+    expect(async () => {
+      await updateCourse.execute({
+        courseId: 'fake-course-id',
+        course: {},
+      });
+    }).rejects.toThrow(CourseNotFound);
+  });
+});
---
+++ src/application/use-cases/courses/update-course.ts
@@ -0,0 +1,34 @@
+import { Description } from '@application/entities/description';
+import { CoursesRepository } from '@application/repositories/courses-repository';
+import { Injectable } from '@nestjs/common';
+import slugify from 'slugify';
+import { CourseNotFound } from '../errors/course-not-found';
+
+interface UpdateCourseRequest {
+  course: {
+    title?: string;
+    description?: Description;
+    imageURL?: string;
+  };
+  courseId: string;
+}
+
+@Injectable()
+export class UpdateCourse {
+  constructor(private coursesRepository: CoursesRepository) {}
+
+  async execute(request: UpdateCourseRequest): Promise<void> {
+    const { course, courseId } = request;
+
+    const findCourse = await this.coursesRepository.findById(courseId);
+
+    if (!findCourse) throw new CourseNotFound();
+
+    if (course.title) findCourse.title = course.title;
+    if (course.title) findCourse.slug = slugify(course.title, { lower: true });
+    if (course.description) findCourse.description = course.description;
+    if (course.imageURL) findCourse.imageURL = course.imageURL;
+
+    await this.coursesRepository.save(findCourse);
+  }
+}
