42c3c04789ec06b2c72f8f900df76a69304a30e4
felipesanderp <felipe.panisson@hotmail.com>
2023-01-27 20:11:45 -0300

    useClass prima-courses-repository with courses repo

--- src/infra/database/database.module.ts
+++ src/infra/database/database.module.ts
@@ -1,7 +1,16 @@
+import { CoursesRepository } from '@application/repositories/courses-repository';
 import { Module } from '@nestjs/common';
 import { PrismaService } from './prisma/prisma.service';
+import { PrismaCoursesRepository } from './prisma/repositories/prisma-courses-repository';
 
 @Module({
-  providers: [PrismaService],
+  providers: [
+    PrismaService,
+    {
+      provide: CoursesRepository,
+      useClass: PrismaCoursesRepository,
+    },
+  ],
+  exports: [CoursesRepository],
 })
 export class DatabaseModule {}
