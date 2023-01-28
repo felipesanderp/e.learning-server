f930214d7aada46fedaa5956a4107a76408a982e
felipesanderp <felipe.panisson@hotmail.com>
2023-01-27 21:46:39 -0300

    import controller and providers

--- src/infra/http/http.module.ts
+++ src/infra/http/http.module.ts
@@ -1,7 +1,14 @@
 import { Module } from '@nestjs/common';
+
 import { DatabaseModule } from '../database/database.module';
+import { CoursesController } from './controllers/courses.controller';
+
+import { GetAllCourses } from '@application/use-cases/courses/get-all-courses';
+import { GetCourseById } from '@application/use-cases/courses/get-course-by-id';
 
 @Module({
   imports: [DatabaseModule],
+  controllers: [CoursesController],
+  providers: [GetAllCourses, GetCourseById],
 })
 export class HttpModule {}
