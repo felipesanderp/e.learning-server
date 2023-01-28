887681d729024e82cfeb2e8d6e789a03bbf068ba
felipesanderp <felipe.panisson@hotmail.com>
2023-01-27 21:46:10 -0300

    course view model to use for response return

---
+++ src/infra/http/view-models/course-view-model.ts
@@ -0,0 +1,14 @@
+import { Course } from '@application/entities/course';
+
+export class CourseViewModel {
+  static toHTTP(course: Course) {
+    return {
+      id: course.id,
+      title: course.title,
+      slug: course.slug,
+      description: course.description,
+      imageURL: course.imageURL,
+      canceledAt: course.canceledAt,
+    };
+  }
+}
