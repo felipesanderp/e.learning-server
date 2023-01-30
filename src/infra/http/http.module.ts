b8d84db65785c309473607c7fa5730a750de19e7
felipesanderp <felipe.panisson@hotmail.com>
2023-01-30 20:43:21 -0300

    provide get all available courses

--- src/infra/http/http.module.ts
+++ src/infra/http/http.module.ts
@@ -6,10 +6,16 @@ import { CoursesController } from './controllers/courses.controller';
 import { GetAllCourses } from '@application/use-cases/courses/get-all-courses';
 import { GetCourseById } from '@application/use-cases/courses/get-course-by-id';
 import { CreateCourse } from '@application/use-cases/courses/create-course';
+import { GetAllAvailableCourses } from '@application/use-cases/courses/get-all-available-courses';
 
 @Module({
   imports: [DatabaseModule],
   controllers: [CoursesController],
-  providers: [GetAllCourses, GetCourseById, CreateCourse],
+  providers: [
+    GetAllCourses,
+    GetCourseById,
+    GetAllAvailableCourses,
+    CreateCourse,
+  ],
 })
 export class HttpModule {}
