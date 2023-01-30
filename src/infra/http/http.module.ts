0398ac5d58115244f01a43914c384acb9061846f
felipesanderp <felipe.panisson@hotmail.com>
2023-01-30 13:50:38 -0300

    provider create course

--- src/infra/http/http.module.ts
+++ src/infra/http/http.module.ts
@@ -5,10 +5,11 @@ import { CoursesController } from './controllers/courses.controller';
 
 import { GetAllCourses } from '@application/use-cases/courses/get-all-courses';
 import { GetCourseById } from '@application/use-cases/courses/get-course-by-id';
+import { CreateCourse } from '@application/use-cases/courses/create-course';
 
 @Module({
   imports: [DatabaseModule],
   controllers: [CoursesController],
-  providers: [GetAllCourses, GetCourseById],
+  providers: [GetAllCourses, GetCourseById, CreateCourse],
 })
 export class HttpModule {}
