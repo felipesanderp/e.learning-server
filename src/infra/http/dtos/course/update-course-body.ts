cac185be57d76f6dfe3c3bde7eef753532ae8384
felipesanderp <felipe.panisson@hotmail.com>
2023-02-02 20:20:47 -0300

    update dto

--- src/infra/http/dtos/course/update-course-body.ts
+++ src/infra/http/dtos/course/update-course-body.ts
@@ -1,13 +1,10 @@
-import { IsNotEmpty, Length } from 'class-validator';
+import { Length } from 'class-validator';
 
 export class UpdateCourseBody {
-  @IsNotEmpty()
   title: string;
 
-  @IsNotEmpty()
   @Length(5, 240)
   description: string;
 
-  @IsNotEmpty()
   imageURL: string;
 }
