f6d91d2208473079e520a3878f28a1795bb3d4d1
felipesanderp <felipe.panisson@hotmail.com>
2023-02-02 20:30:07 -0300

    update response

--- src/application/use-cases/courses/update-course.ts
+++ src/application/use-cases/courses/update-course.ts
@@ -1,8 +1,15 @@
-import { Description } from '@application/entities/description';
-import { CoursesRepository } from '@application/repositories/courses-repository';
 import { Injectable } from '@nestjs/common';
 import slugify from 'slugify';
+
+import { Course } from '@application/entities/course';
+import { Description } from '@application/entities/description';
+import { CoursesRepository } from '@application/repositories/courses-repository';
+
 import { CourseNotFound } from '../errors/course-not-found';
+
+interface UpdateCourseResponse {
+  course: Course;
+}
 
 interface UpdateCourseRequest {
   course: {
@@ -17,7 +24,7 @@ interface UpdateCourseRequest {
 export class UpdateCourse {
   constructor(private coursesRepository: CoursesRepository) {}
 
-  async execute(request: UpdateCourseRequest): Promise<void> {
+  async execute(request: UpdateCourseRequest): Promise<UpdateCourseResponse> {
     const { course, courseId } = request;
 
     const findCourse = await this.coursesRepository.findById(courseId);
@@ -31,5 +38,9 @@ export class UpdateCourse {
     if (course.imageURL) findCourse.imageURL = course.imageURL;
 
     await this.coursesRepository.save(findCourse);
+
+    return {
+      course: findCourse,
+    };
   }
 }
