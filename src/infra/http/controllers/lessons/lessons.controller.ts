f8406a878bfc87c016508854c7f52ffe06fbd79c
Felipe Sander <felipe.panisson@hotmail.com>
2023-02-07 17:05:04 -0300

    lessons controller, with create route

--- src/infra/http/controllers/lessons/lessons.controller.ts
+++ src/infra/http/controllers/lessons/lessons.controller.ts
@@ -1,4 +1,25 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+
+import { CreateLesson } from '@application/use-cases/lessons/create-lesson';
+
+import { CreateLessonBody } from '@infra/http/dtos/lessons/create-lesson-body';
+import { LessonViewModel } from '@infra/http/view-models/lesson-view-model';
 
 @Controller('lessons')
-export class LessonsController {}
+export class LessonsController {
+  constructor(private createLesson: CreateLesson) {}
+
+  @Post()
+  async create(@Body() body: CreateLessonBody) {
+    const { name, description, duration, video_id } = body;
+
+    const { lesson } = await this.createLesson.execute({
+      name,
+      description,
+      duration,
+      video_id,
+    });
+
+    return { lesson: LessonViewModel.toHTTP(lesson) };
+  }
+}
