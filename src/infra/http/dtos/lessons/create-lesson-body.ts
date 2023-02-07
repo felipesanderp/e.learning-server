8ab601bacb0d7561cd5963187fa3487b32fca505
Felipe Sander <felipe.panisson@hotmail.com>
2023-02-07 17:05:22 -0300

    create lesson body dto

---
+++ src/infra/http/dtos/lessons/create-lesson-body.ts
@@ -0,0 +1,17 @@
+import { IsNotEmpty, Length } from 'class-validator';
+
+export class CreateLessonBody {
+  @IsNotEmpty()
+  @Length(5, 240)
+  name: string;
+
+  @IsNotEmpty()
+  @Length(5, 240)
+  description: string;
+
+  @IsNotEmpty()
+  duration: number;
+
+  @IsNotEmpty()
+  video_id: string;
+}
