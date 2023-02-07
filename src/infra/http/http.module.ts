b7f97bbec65a49c2fe2cde9266a0d6bb934521d1
Felipe Sander <felipe.panisson@hotmail.com>
2023-02-07 16:52:11 -0300

    lessons controller

--- src/infra/http/http.module.ts
+++ src/infra/http/http.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 
 import { DatabaseModule } from '../database/database.module';
 import { CoursesController } from './controllers/courses/courses.controller';
+import { LessonsController } from './controllers/lessons/lessons.controller';
 
 import { GetAllCourses } from '@application/use-cases/courses/get-all-courses';
 import { GetCourseById } from '@application/use-cases/courses/get-course-by-id';
@@ -12,7 +13,7 @@ import { UpdateCourse } from '@application/use-cases/courses/update-course';
 
 @Module({
   imports: [DatabaseModule],
-  controllers: [CoursesController],
+  controllers: [CoursesController, LessonsController],
   providers: [
     GetAllCourses,
     GetCourseById,
