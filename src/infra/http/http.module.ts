3d461099cf6e5dfecef2e3ced5b7e233390d5de5
Felipe Sander <felipe.panisson@hotmail.com>
2023-02-15 17:27:36 -0300

    import lessons providers

--- src/infra/http/http.module.ts
+++ src/infra/http/http.module.ts
@@ -11,6 +11,14 @@ import { CancelCourse } from '@application/use-cases/courses/cancel-course';
 import { RemoveCourse } from '@application/use-cases/courses/remove-course';
 import { UpdateCourse } from '@application/use-cases/courses/update-course';
 
+import { CreateLesson } from '@application/use-cases/lessons/create-lesson';
+import { GetAllLessons } from '@application/use-cases/lessons/get-all-lessons';
+import { GetLessonByCourseId } from '@application/use-cases/lessons/get-lesson-by-course-id';
+import { GetAllAvailableLessons } from '@application/use-cases/lessons/get-all-available-lessons';
+import { CancelLesson } from '@application/use-cases/lessons/cancel-lesson';
+import { RemoveLesson } from '@application/use-cases/lessons/remove-lesson';
+import { UpdateLesson } from '@application/use-cases/lessons/update-lesson';
+
 @Module({
   imports: [DatabaseModule],
   controllers: [CoursesController, LessonsController],
@@ -21,6 +29,14 @@ import { UpdateCourse } from '@application/use-cases/courses/update-course';
     UpdateCourse,
     CancelCourse,
     RemoveCourse,
+
+    CreateLesson,
+    GetAllLessons,
+    GetLessonByCourseId,
+    GetAllAvailableLessons,
+    CancelLesson,
+    RemoveLesson,
+    UpdateLesson,
   ],
 })
 export class HttpModule {}
